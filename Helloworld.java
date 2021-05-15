2a342f5f49c04dbd858aed77463b6f8907e9f695
Ajay Jangid <65307820+ajayjangid06@users.noreply.github.com>
2021-05-15 12:34:07 +0530

    Add files via upload
    committed

---
+++ Helloworld.java
@@ -0,0 +1,18 @@
+public class Helloworld
+{
+    public static void main(String [ ] arg) 
+    {
+        char ch = '\ubced';
+        System.out.println("Hello!");
+        System.out.println("Welcome to the world of Java.");
+        System.out.println(ch);
+      int a = 0;  
+    if(a==0){
+        System.out.println("TRUE");
+    }
+    else{
+        System.out.println("FALSE");
+    }
+    }
+}
+
